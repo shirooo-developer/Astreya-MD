cd2c67d2684a5d7921f602e05fdeca2abae348f9
Shirooo Official <107530906+shirooo-developer@users.noreply.github.com>
2023-06-29 17:00:34 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -71,16 +71,25 @@ blocked.push(i.replace('c.us','s.whatsapp.net'))}})
 XeonBotInc.ev.on('messages.upsert', async chatUpdate => {
   try {
     kay = chatUpdate.messages[0]
-    if (!kay.message) return False
-    kay.message = (Object.keys(kay.message)[0] === 'ephemeralMessage') ? kay.message.ephemeralMessage.message : kay.message
-    if (kay.key && kay.key.remoteJid === 'status@broadcast') return False
-    if (!XeonBotInc.public && !kay.key.fromMe && chatUpdate.type === 'notify') return False
-    if (kay.key.id.startsWith('BAE5') && kay.key.id.length === 16) return False
+    if (!kay.message) {
+      return False
+    }
+    kay.message = (Object.keys(kay.message)[0] === 'ephemeralMessage')
+      ? kay.message.ephemeralMessage.message
+      : kay.message
+    if (kay.key && kay.key.remoteJid === 'status@broadcast') {
+      return False
+    }
+    if (!XeonBotInc.public && !kay.key.fromMe && chatUpdate.type === 'notify') {
+      return False
+    }
+    if (kay.key.id.startsWith('BAE5') && kay.key.id.length === 16) {
+      return False
+    }
     m = smsg(XeonBotInc, kay, store)
     require('./XeonCheems7')(XeonBotInc, m, chatUpdate, store)
   } catch (err) {
-    console.log(err)
-  })
+    console.log(err)}})
 
 	// detect group update
 		XeonBotInc.ev.on("groups.update", async (json) => {
